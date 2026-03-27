096a7a6b3bb1fcc30ac6ce7d87a9627cd1524e63
lishilong <lishilong@bytedance.com>
2026-03-27 16:13:34 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+RUN_ID=baseline_8gpu \
+DATA_PATH=./data/datasets/fineweb10B_sp1024/ \
+TOKENIZER_PATH=./data/tokenizers/fineweb_1024_bpe.model \
+VOCAB_SIZE=1024 \
+torchrun --standalone --nproc_per_node=8 train_gpt.py
